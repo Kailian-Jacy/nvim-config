884d502dacfafc6424d21055bc5c43fe9832a0ac
Xu zian <xza1809884911@163.com>
2024-06-26 13:09:22 +0800

    Dependencies

--- prebuild.sh
+++ prebuild.sh
@@ -1,15 +1,27 @@
-#!/bin/bash -e
+#!/bin/zsh -e
+
+BREW="HOMEBREW_NO_AUTO_UPDATE=1 brew"
 
 function install_neovim() {
-	HOMEBREW_NO_AUTO_UPDATE=1 brew install neovim
+	$BREW install neovim
+ 	echo "alias vim=nvim" >> ~/.zshrc
+  	source ~/.zshrc
 }
 
-CONFIG_SOURCE=$(dirname "$0")/
+CURRENT_ABS=${0:a}
+CONFIG_SOURCE=$(dirname $CURRENT_ABS)/
 
 function clone_config() {
 	mkdir -p ~/.config
 	ln -s CONFIG_SOURCE/config.nvim ~/.config/nvim
+
+}
+
+function dependencies() {
+ 	$BREW install node
+	nvim
 }
 
 install_neovim
 clone_config
+dependencies
