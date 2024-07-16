cca89ca3b29123c4c6601ccffee6e904dfbce7d3
xuzianxu_i <xuzianxu_i@didiglobal.com>
2024-07-16 17:45:01 +0800

    fix: auto save and formatting

--- config.nvim/lua/config/keymaps.lua
+++ config.nvim/lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("v", "/", '"fy/\\V<C-R>f<CR>')
-vim.keymap.set("n", "<leader>s", "<cmd>w<CR>")
+--vim.keymap.set("n", "<leader>s", "<cmd>w<CR>") -- deprecated. Now use auto save.
 vim.keymap.set("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
--- config.nvim/lua/config/options.lua
+++ config.nvim/lua/config/options.lua
@@ -3,3 +3,4 @@
 -- Add any additional options here
 vim.g.minimap_highlight_search = 1
 vim.g.minimap_git_colors = 1
+vim.g.autoformat = false
