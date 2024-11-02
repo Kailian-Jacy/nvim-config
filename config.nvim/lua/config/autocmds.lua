dd031ea73f8e5627a0c990d39532bdbd2904ab32
kailian jacy <xza1809884911@163.com>
2024-11-02 17:42:56 +0800

    fix: cursor remains block after leaving vim. Workaround
    Now perfect for cursor in terminal inside vim. Still block

--- config.nvim/lua/config/autocmds.lua
+++ config.nvim/lua/config/autocmds.lua
@@ -1,6 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- Trigger linter
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     callback = function()
         -- try_lint without arguments runs the linters defined in `linters_by_ft`
@@ -11,6 +12,10 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         -- run specific linters, independent of the `linters_by_ft` configuration
         require("lint").try_lint("cspell")
     end,
+})
+-- Workaround for a tmux problem:
+vim.api.nvim_create_autocmd("VimLeave", {
+    command = "set guicursor=a:ver1",
 })
 -- dap close float window on esc
 vim.api.nvim_create_autocmd("FileType", {
