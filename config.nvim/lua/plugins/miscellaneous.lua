99715940bf615367097215a3e56caaea69b8d8b8
kailian jacy <xza1809884911@163.com>
2025-02-12 19:18:10 +0800

    callbacking telescope: bookmark requires it

--- config.nvim/lua/plugins/miscellaneous.lua
+++ config.nvim/lua/plugins/miscellaneous.lua
@@ -345,7 +345,6 @@ return {
   -- change some telescope options and a keymap to browse plugin files
   {
     "nvim-telescope/telescope.nvim",
-    enabled = false,
     keys = {
       -- {
       --   "<leader>ff",
