b307b1d2419c94f9e38fc39b2bafa32f08246006
kailian jacy <xza1809884911@163.com>
2025-02-22 12:09:47 +0800

    keymap: remove dependency for c-w

--- config.nvim/lua/config/keymaps.lua
+++ config.nvim/lua/config/keymaps.lua
@@ -41,15 +41,12 @@ end, { noremap = true })
 vim.keymap.set("n", "<leader>G", "<cmd>LazyGit<CR>", { noremap = true, silent = true })
 
 -- Navigation: Cursor wandering around
-vim.keymap.set({ "n", "v" }, "<C-J>", "<C-W>j", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<C-H>", "<C-W>h", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<C-L>", "<C-W>l", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "i" }, "<C-J>", "<cmd>wincmd j<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "i" }, "<C-H>", "<cmd>wincmd h<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "i" }, "<C-L>", "<cmd>wincmd l<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "i" }, "<C-K>", "<cmd>wincmd k<cr>", { noremap = true, silent = true })
+-- vim.keymap.set({ "n", "v", "i" }, "<C-BS>", "<cmd>wincmd p<cr>", { noremap = true, silent = true }) --it won't go across tabs. useless.
 vim.keymap.set({ "t" }, "<C-L>", "<C-L>", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<C-K>", "<C-W>k", { noremap = true, silent = true })
-vim.keymap.set({ "i" }, "<C-J>", "<esc><C-W>jli", { noremap = true, silent = true })
-vim.keymap.set({ "i" }, "<C-H>", "<esc><C-W>hli", { noremap = true, silent = true })
-vim.keymap.set({ "i" }, "<C-L>", "<esc><C-W>lli", { noremap = true, silent = true })
-vim.keymap.set({ "i" }, "<C-K>", "<esc><C-W>kli", { noremap = true, silent = true })
 
 -- search
 vim.keymap.set("v", "/", '"fy/\\V<C-R>f<CR>')
