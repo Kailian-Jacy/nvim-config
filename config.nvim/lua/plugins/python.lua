fec718626c66ec90a1a5b3aa1cf7b944b3e6f4f9
kailian jacy <xza1809884911@163.com>
2025-03-22 18:11:26 +0800

    disable venv.

--- config.nvim/lua/plugins/python.lua
+++ config.nvim/lua/plugins/python.lua
@@ -1,9 +1,10 @@
 return {
   {
     "linux-cultist/venv-selector.nvim",
+    enabled = false,
     dependencies = {
       "neovim/nvim-lspconfig",
-      "nvim-telescope/telescope.nvim",
+      -- "nvim-telescope/telescope.nvim",
       "mfussenegger/nvim-dap-python",
     },
     -- Selector is not needed except wanting to point
