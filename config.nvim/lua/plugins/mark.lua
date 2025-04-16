61640a723d444022d04002a69a7049d2a4ac3b59
Xu Zian <xuzian@XudeMacBook-Pro.local>
2025-04-16 17:09:09 +0800

    trial: remove image support in neovim.

--- config.nvim/lua/plugins/mark.lua
+++ config.nvim/lua/plugins/mark.lua
@@ -1,15 +1,3 @@
 -- Plugins about marked-up languages goes here.
 -- markdown, latex, etc.
-return {
-  {
-    "HakonHarnes/img-clip.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- add options here
-      -- or leave it empty to use the default settings
-    },
-    keys = {
-      -- keymap to paste image. Compatible with obsidian plugins, not set here. See keymap.lua
-    },
-  }
-}
+return {}
