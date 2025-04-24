918fcd1559d979abb04fc5a43c274066e8284da2
Xu Zian <xuzian@XudeMacBook-Pro.local>
2025-04-24 10:20:23 +0800

    pin lualine version.

--- config.nvim/lua/plugins/theme.lua
+++ config.nvim/lua/plugins/theme.lua
@@ -133,6 +133,7 @@ return {
   -- the opts function can also be used to change the default opts:
   {
     "nvim-lualine/lualine.nvim",
+    commit = "86fe395",
     event = "VeryLazy",
     --[[opts = function(_, opts)
       local trouble = require("trouble")
