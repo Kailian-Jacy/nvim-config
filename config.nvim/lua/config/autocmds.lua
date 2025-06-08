862de1b06b2e006f656a65e3da6799ef1d968a5a
kailian jacy <xza1809884911@163.com>
2025-06-08 20:48:19 +0800

    revert shada write.

--- config.nvim/lua/config/autocmds.lua
+++ config.nvim/lua/config/autocmds.lua
@@ -266,14 +266,14 @@ end, { desc = "List all svn modifications." })
 
 -- Oldfiles related.
 -- Save & load the pages in record after entering buffer.
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  -- Async write and load shada files.
-  callback = vim.schedule_wrap(function()
-    vim.cmd([[ wshada ]])
-    vim.cmd([[ rshada! ]])
-  end),
-})
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = "*",
+--   -- Async write and load shada files.
+--   callback = vim.schedule_wrap(function()
+--     vim.cmd([[ wshada ]])
+--     vim.cmd([[ rshada! ]])
+--   end),
+-- })
 
 -- Quickfix related.
 -- Page closing
