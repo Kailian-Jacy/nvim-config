869cb9f043dbb53f4d6162dd901a6e64379ef3d3
Kailian Jacy <xza1809884911@163.com>
2025-06-10 11:24:10 +0800

    theme: dim ignore quickfix

--- config.nvim/lua/plugins/theme.lua
+++ config.nvim/lua/plugins/theme.lua
@@ -336,7 +336,7 @@ return {
           },
           -- Still problematic. AvanteSidebarWinHorizontalSeparator will be hidden.
           -- buf_opts = { filetype = { "Avante", "AvanteSelectedFiles" } },
-          buf_opts = { filetype = { "noice" } },
+          buf_opts = { filetype = { "noice", "qf" } },
         },
       },
     },
