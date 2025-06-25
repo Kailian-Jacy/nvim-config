761cfca8094f0765225eb16dccf5ed405886914a
Kailian Jacy <xza1809884911@163.com>
2025-06-25 18:31:18 +0800

    config: local.lua add hooks

--- config.nvim/init.lua
+++ config.nvim/init.lua
@@ -1,11 +1,43 @@
+-- Sometimes we want to have different settings across nvim deployments.
+-- Load the local config to set something locally.
+local success, local_funcs = pcall(require, "config.local")
+
+if success and local_funcs.before_all and type(local_funcs.before_all) == "function" then
+  local_funcs.before_all()
+end
+
 local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
 vim.cmd.source(vimrc)
 
+-- load options.
 require("config.options")
-require("config.lazy")
-require("config.autocmds")
-require("config.keymaps")
+if success and local_funcs.after_options and type(local_funcs.after_options) == "function" then
+  local_funcs.after_options()
+end
 
--- Sometimes we want to have different settings across nvim deployments.
--- Load the local config to set something locally.
-local _, _ = pcall(require, "config.local")
+-- load plugins.
+if success and local_funcs.before_plugins_load and type(local_funcs.before_plugins_load) == "function" then
+  local_funcs.before_plugins_load()
+end
+require("config.lazy")
+if success and local_funcs.after_plugins_load and type(local_funcs.after_plugins_load) == "function" then
+  local_funcs.after_plugins_load()
+end
+
+-- Load autocmds
+if success and local_funcs.before_autocmds and type(local_funcs.before_autocmds) == "function" then
+  local_funcs.before_autocmds()
+end
+require("config.autocmds")
+if success and local_funcs.after_autocmds and type(local_funcs.after_autocmds) == "function" then
+  local_funcs.after_autocmds()
+end
+
+-- Load keymaps.
+if success and local_funcs.before_keymaps and type(local_funcs.before_keymaps) == "function" then
+  local_funcs.before_keymaps()
+end
+require("config.keymaps")
+if success and local_funcs.after_all and type(local_funcs.after_all) == "function" then
+  local_funcs.after_all()
+end
