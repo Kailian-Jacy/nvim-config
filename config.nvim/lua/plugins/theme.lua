9ab4ef6c90ea4f1e8d3796122a92eebd02d8badf
Zian Xu <zianxu@deepseek.com>
2026-06-21 20:43:09 +0800

    Revert "Fix statusline: show terminal title instead of previous buffer path"
    This reverts commit 46142a5b8f153cb597257afa95baf6fe594840e7.

--- config.nvim/lua/plugins/theme.lua
+++ config.nvim/lua/plugins/theme.lua
@@ -298,21 +298,7 @@ return {
             end,
           }}, -- Used to display is Debugging information.]]
           lualine_a = {
-            {
-              function()
-                local buf = vim.api.nvim_get_current_buf()
-                local bt = vim.bo[buf].buftype
-                if bt == "terminal" then
-                  local title = vim.b[buf].term_title
-                  if title and title ~= "" then
-                    title = title:match("([^/]+)$") or title
-                    return " " .. title
-                  end
-                  return " terminal"
-                end
-                return vim.fn.expand("%:~:.")
-              end,
-            },
+            { "filename", path = 1 },
           },
           lualine_b = {},
           lualine_c = {},
